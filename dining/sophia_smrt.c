2804d4a7362b4d24fa5bbedeec5112ecefd8ef64
Bill <billebeling@gmail.com>
2013-12-09 08:18:05 -0500

    added _smrt to files, cleaned up sophia.c

--- dining/sophia_smrt.c
+++ dining/sophia_smrt.c
@@ -2,9 +2,8 @@
 #include <pthread.h>
 #include <time.h>
 #include <semaphore.h>
-#include <unistd.h>
 #include "dining_list.h"
-
+#include <unistd.h>
 
 /**
   * Bill Ebeling
@@ -16,9 +15,18 @@ enum {FALSE, TRUE};
 enum {THINKING, HUNGRY, EATING};
 
 pthread_mutex_t chops[N];
+pthread_mutex_t singlepass;
+pthread_cond_t eaters[N];
 
 // running is universal so that when a thread gets to numRounds, they all stop
-int running=TRUE, numRounds=200;
+int running=TRUE, numRounds=200000000;
+int nextUp;
+
+LIST waitingList; 
+ELEMENT NULLELEM;
+struct {ELEMENT firstEater; 
+        ELEMENT secondEater;
+} diners;
 
 int isFull(){
   // % chance
@@ -28,23 +36,23 @@ int isFull(){
 
 int isHungry(){
   // % chance
-  if (rand()%100 < 5) return TRUE;
+  if (rand()%100 < 10) return TRUE;
   else return FALSE;
 }
 
-void reserveUtensils(int myID){
-  if (myID % 2 == 0){//even looks right first 
-    pthread_mutex_lock(&chops[(myID+1) % N]);
-    pthread_mutex_lock(&chops[myID]);
-  }else{//odd looks left first
-    pthread_mutex_lock(&chops[myID]);
-    pthread_mutex_lock(&chops[(myID+1) % N]);
-  }
+void removeDiner(ELEMENT elem){
+  if(diners.firstEater.myID == elem.myID) diners.firstEater = NULLELEM;
+  else if (diners.secondEater.myID == elem.myID) diners.secondEater = NULLELEM;
 }
 
-void releaseUtensils(int myID){
-  pthread_mutex_unlock(&chops[myID]);
-  pthread_mutex_unlock(&chops[(myID+1) % N]);
+void reserveUtensils(int myID){
+    pthread_mutex_lock(&singlepass);
+    printf("\033[%d;%dH %d first mutex!--cond: %d", 20 , 30*myID, myID, (myID != nextUp ));
+    while (myID != nextUp ) pthread_cond_wait(&eaters[myID], &singlepass);
+    //while (1 != 1) pthread_cond_wait(&eaters[myID], &singlepass);
+    printf("\033[%d;%dH %d second mutex!", 21, 30*myID, myID);
+    pthread_mutex_unlock(&singlepass);
+    printf("\033[%d;%dH %d in crit region!", 22, 30*myID, myID);    
 }
 
 /** 
@@ -78,7 +86,10 @@ void *philosopher(void * id){
 
 	    case HUNGRY:
 	      hungerUnits++;
+        listAddElem(waitingList, me);
 	  	  reserveUtensils(me.myID);
+        if(diners.firstEater.myID == 9) diners.firstEater = me;
+        else if (diners.secondEater.myID == 9) diners.secondEater = me;
 	      msg = "has begun eating!";
 	      state=EATING;
 	      break;
@@ -86,8 +97,15 @@ void *philosopher(void * id){
 	    case EATING:
 	      eatingUnits++;
 	      if (isFull() == TRUE){
-		      releaseUtensils(me.myID);
+		      //releaseUtensils(me.myID);
+          nextUp = listGetNext(waitingList, me);
+          listRemoveElem(waitingList, me);
     		  state=THINKING;
+          if (diners.firstEater.myID == myID) diners.firstEater = NULLELEM;
+          else if (diners.secondEater.myID == myID) diners.secondEater = NULLELEM;
+
+          pthread_cond_broadcast(&eaters[(myID + 1) % N]);
+
 	      	msg="has had their full!";
 	      } else {
       		msg="currently eating";
@@ -100,9 +118,9 @@ void *philosopher(void * id){
 	  }
 
 	  // this funny line moves the cursor around so the screen updates without scrolling  
-	  // MAY NOT BE WINDOWS COMPLIANT
 	  printf("\033[%d;%dH%-15d%-15d%-15d%-15d%-15d%-15d%-30s\033[20;5H", 
 		  (myID+3), 0, myID, runCount, state, thinkUnits, hungerUnits, eatingUnits, msg);
+	  listPrint(waitingList);
 
 	  msg="";
 	  if(runCount % 10 == 0) fflush(stdout);  
@@ -112,13 +130,19 @@ void *philosopher(void * id){
 
 	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	// if  EATING when it stops, release utensils so others can unblock and exit their loops
-	if (EATING) releaseUtensils(me.myID);
+	//if (EATING) releaseUtensils(me.myID);
+  if (EATING){
+    if (diners.firstEater.myID == myID) diners.firstEater = NULLELEM;
+    else if (diners.secondEater.myID == myID) diners.secondEater = NULLELEM;
+    nextUp = listGetNext(waitingList, me);
+  }
 }
 
 // MAIN
 int main(){
   pthread_t thr[N];
   int ids[N];
+  nextUp = 0;
 
   srand((long int) time(NULL));
 
@@ -126,12 +150,19 @@ int main(){
   printf("%-15s%-15s%-15s%-15s%-15s%-15s%-15s",
 	     "id","TRound","state","ThinkUnits","hungerUnits","EatingUnits","message");
 
+  waitingList = listCreate(N);
+  ELEMENT NULLELEM = {9,9,9};
+  diners.firstEater = NULLELEM;
+  diners.secondEater= NULLELEM;
+
+  pthread_mutex_init( &singlepass, NULL );
+
   int i;
   for (i=0; i < N; i++){
-    pthread_mutex_init( &chops[i], NULL); 
     ids[i] = i;
     usleep(100000);
     pthread_mutex_init( &chops[ids[i]], NULL); 
+    pthread_cond_init( &eaters[ids[i]], NULL );
     pthread_create(thr + i, NULL, philosopher, (void*)(ids+i));
   }
   
@@ -142,6 +173,8 @@ int main(){
   for (i = 0; i < N; i++ ){
     pthread_mutex_destroy(&chops[i]);
   }
+
+  pthread_mutex_destroy( &singlepass );
 
   printf("OK, bye\n");
   return 0;
